e4ff63449ba09e96fb81cb51d12d79be97395796
haquynh91 <103050487+haquynh91@users.noreply.github.com>
2022-04-28 01:19:50 +0700

    Create script.js

---
+++ js-1/script.js
@@ -0,0 +1,9 @@
+function thongbao() {
+  alert("Welcome to Techmaster!");
+}
+function thongbao2(x) {
+  alert("Hello " + x + ". Welcome to Techmaster!");
+}
+function binhphuong(x) {
+  return (x * x)
+}
